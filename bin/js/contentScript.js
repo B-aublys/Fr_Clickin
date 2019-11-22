e541fe03815cc9b39b11833ccbb14d498586940e
NeonHands <workingneonhands@gmail.com>
2019-11-22 19:59:56 +0200

    From Now using webpack to pack all the js files moved a lot of stuff into the dist folder

--- bin/js/contentScript.js
+++ bin/js/contentScript.js
@@ -3,20 +3,20 @@ let elementObject
 // array to store all the running clickers by their ID
 let clickers = {}
 
-function messageHandler(request, sender, sendResponse){
-	switch (request.ID){
+function messageHandler(request, sender, sendResponse) {
+	switch (request.ID) {
 		case "selected":
 			WriteNewClicker()
 			break
-		
+
 		case "website":
-			sendResponse({website: window.location.hostname})
+			sendResponse({ website: window.location.hostname })
 			break
-		
+
 		case "restartClicker":
 			restartClicker(request.clicker)
 			break
-		
+
 		case "removeClicker":
 			removeClicker(request.clickerID)
 			break
@@ -29,64 +29,64 @@ function messageHandler(request, sender, sendResponse){
 
 
 // Gets the id and xpath of an element
-function getElement(element){
+function getElement(element) {
 	let id = element.id
-	let {relativeID, xpath} = getXPathOfElement(element)
-	return {id, relativeID, xpath}
-}   
+	let { relativeID, xpath } = getXPathOfElement(element)
+	return { id, relativeID, xpath }
+}
 
 
-function getXPathOfElement(el){
+function getXPathOfElement(el) {
 	let xpath = ""
 	// relate from holds the id from which the xpath is relative from
-    let relativeID, tempEl, pos
+	let relativeID, tempEl, pos
 
-    while(el){
-        pos = 0 
-        tempEl = el
+	while (el) {
+		pos = 0
+		tempEl = el
 
-        if (el.id){
-            relativeID = el.id
-            xpath = "." + xpath
-            break
-        }
-        
-        while(tempEl){
-            if(tempEl.nodeType ===1 && tempEl.nodeName === el.nodeName){
-                pos+=1
-            }
-            tempEl = tempEl.previousSibling
-        }
-        
+		if (el.id) {
+			relativeID = el.id
+			xpath = "." + xpath
+			break
+		}
 
-        xpath = "/" + el.tagName.toLowerCase() + "[" + pos + "]" + xpath
-        el = el.parentElement
+		while (tempEl) {
+			if (tempEl.nodeType === 1 && tempEl.nodeName === el.nodeName) {
+				pos += 1
+			}
+			tempEl = tempEl.previousSibling
+		}
+
+
+		xpath = "/" + el.tagName.toLowerCase() + "[" + pos + "]" + xpath
+		el = el.parentElement
 	}
-    return {relativeID, xpath}
+	return { relativeID, xpath }
 }
 
 // MDN snippet for evaluating an xpath expresion and returning it's founds
 function evaluateXPath(aNode, aExpr) {
 	var xpe = new XPathEvaluator();
 	var nsResolver = xpe.createNSResolver(aNode.ownerDocument == null ?
-	  aNode.documentElement : aNode.ownerDocument.documentElement);
+		aNode.documentElement : aNode.ownerDocument.documentElement);
 	var result = xpe.evaluate(aExpr, aNode, nsResolver, 0, null);
 	var found = [];
 	var res;
 	while (res = result.iterateNext())
-	  found.push(res);
+		found.push(res);
 	return found;
 }
 
 
 // FIXME: rename this function to something comprehensive
-function getClickers(){
+function getClickers() {
 	browser.storage.local.get([window.location.hostname], clickerObjectJSON => {
-		if(clickerObjectJSON[window.location.hostname]){
+		if (clickerObjectJSON[window.location.hostname]) {
 			let clickerObject = JSON.parse(clickerObjectJSON[window.location.hostname])
-			for (let [clickerNr, clicker]  of Object.entries(clickerObject)){
-				if(clicker.active){
-					startClicker(clicker)
+			for (let [clickerNr, clicker] of Object.entries(clickerObject)) {
+				if (clicker.active) {
+					startClicker(clickerNr, clicker)
 				}
 			}
 		}
@@ -94,13 +94,13 @@ function getClickers(){
 }
 
 
-function generateNewClickerID(currentIDs, length){
-	while(true) {
+function generateNewClickerID(currentIDs, length) {
+	while (true) {
 		let generated = '';
 		let characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
 		let charactersLength = characters.length;
-		for ( var i = 0; i < length; i++ ) {
-		   generated = generated + characters.charAt(Math.floor(Math.random() * charactersLength));
+		for (var i = 0; i < length; i++) {
+			generated = generated + characters.charAt(Math.floor(Math.random() * charactersLength));
 		}
 
 		if (currentIDs.indexOf(generated) == -1) {
@@ -110,34 +110,34 @@ function generateNewClickerID(currentIDs, length){
 }
 
 
-function restartClicker(clicker){
+function restartClicker(clicker) {
 	let clickerID = Object.keys(clicker)[0]
 	clearInterval(clickers[clickerID])
 	startClicker(clickerID, clicker[clickerID])
 }
 
-function removeClickers(){
-	for (let [clickerNr, clicker]  of Object.entries(clickers)){
+function removeClickers() {
+	for (let [clickerNr, clicker] of Object.entries(clickers)) {
 		removeClicker(clickerNr)
 	}
 }
 
 
-function removeClicker(clickerID){
+function removeClicker(clickerID) {
 	clearInterval(clickers[clickerID])
 }
 
 // Find the element that needs to be clicked
 // creates a new entry in the clickers list
-function startClicker(clickerID, clickerObject){
+function startClicker(clickerID, clickerObject) {
 	let elementToClick
 
-	if (clickerObject.active){
+	if (clickerObject.active) {
 		let interval = clickerObject.interval
-		if (clickerObject.intervalStep == "s"){
+		if (clickerObject.intervalStep == "s") {
 			interval *= 1000
 		}
-		if (clickerObject.elemID){
+		if (clickerObject.elemID) {
 			elementToClick = document.getElementById(clickerObject.elemID)
 		} else {
 			let xpathObjects = evaluateXPath(clickerObject.relativeID ? document.getElementById(clickerObject.relativeID) : document, clickerObject.xpath)
@@ -146,12 +146,12 @@ function startClicker(clickerID, clickerObject){
 			//TODO: more than 1 xpath handling
 			if (xpathObjects.length > 1) {
 				elementToClick = xpathObjects[0]
-				
+
 			} else {
 				console.log("xpath found more that 1 object")
 			}
 		}
-		if (elementToClick){
+		if (elementToClick) {
 			clickers[clickerID] = (setInterval(() => elementToClick.click(), parseInt(interval)))
 		}
 	}
@@ -163,46 +163,46 @@ function startClicker(clickerID, clickerObject){
 // Since the storage is stored based on the sites URI (to avoid dynamic links...)
 // If there is an Item with the same ID or xpath it will be considered as 1
 // Don't know what kind of effect it will have on the usability, but I think very minimal
-function WriteNewClicker(message){
-	let {id, relativeID, xpath} = getElement(elementObject)
+function WriteNewClicker(message) {
+	let { id, relativeID, xpath } = getElement(elementObject)
 	let currentClickerIDlist = []
-	let newClicker = { 
-	  "elemID": id,
-	  "name": id? id:relativeID,
-	  "relativeID": relativeID,
-	  "xpath": xpath,
-	  "interval": 1,
-	  "intervalStep": "s",
-	  "active": false
+	let newClicker = {
+		"elemID": id,
+		"name": id ? id : relativeID,
+		"relativeID": relativeID,
+		"xpath": xpath,
+		"interval": 1,
+		"intervalStep": "s",
+		"active": false
 	}
 
 	browser.storage.local.get(window.location.hostname, response => {
-	  if (response[window.location.hostname]){
-		let responseValue = JSON.parse(response[window.location.hostname])
-		
-		
-		// If there is already an elemen with the same ID or xpath don't generate
-		// a new item
-		for(let [itemNr, item] of Object.entries(responseValue)){
-			currentClickerIDlist.push(itemNr)
-		  	if ((item["elemID"] == newClicker["elemID"] && newClicker["elemID"] != "") 
-			  || ((item["xpath"] == newClicker["xpath"] && newClicker['xpath'] != "") &&
-			  item["relativeID"] == newClicker["relativeID"] && newClicker["relativeID"] != "")){
-			return
-		  	}
-		}
+		if (response[window.location.hostname]) {
+			let responseValue = JSON.parse(response[window.location.hostname])
 
-		// Generate a unique ID for the clickerID
-		// Used for deleting, restarting the clicker without reseting everyhting
-		let clickerID = generateNewClickerID(currentClickerIDlist, 5)
-	  // If there are no duplicates add the new item to the list
-	  responseValue[clickerID] = newClicker
-	  browser.storage.local.set({[window.location.hostname]: JSON.stringify(responseValue)})
-	  
-	  } else {
-		let clickerID = generateNewClickerID(currentClickerIDlist, 5)  
-		browser.storage.local.set({[window.location.hostname]: JSON.stringify({[clickerID]: newClicker})})
-	  } 
+
+			// If there is already an elemen with the same ID or xpath don't generate
+			// a new item
+			for (let [itemNr, item] of Object.entries(responseValue)) {
+				currentClickerIDlist.push(itemNr)
+				if ((item["elemID"] == newClicker["elemID"] && newClicker["elemID"] != "")
+					|| ((item["xpath"] == newClicker["xpath"] && newClicker['xpath'] != "") &&
+						item["relativeID"] == newClicker["relativeID"] && newClicker["relativeID"] != "")) {
+					return
+				}
+			}
+
+			// Generate a unique ID for the clickerID
+			// Used for deleting, restarting the clicker without reseting everyhting
+			let clickerID = generateNewClickerID(currentClickerIDlist, 5)
+			// If there are no duplicates add the new item to the list
+			responseValue[clickerID] = newClicker
+			browser.storage.local.set({ [window.location.hostname]: JSON.stringify(responseValue) })
+
+		} else {
+			let clickerID = generateNewClickerID(currentClickerIDlist, 5)
+			browser.storage.local.set({ [window.location.hostname]: JSON.stringify({ [clickerID]: newClicker }) })
+		}
 	})
 }
 
