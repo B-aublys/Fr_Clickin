053022ccbcedc37173f8447816bd2b3eb1c55886
NeonHands <workingneonhands@gmail.com>
2019-11-24 19:52:49 +0200

    Fixed the webpack problem, webpack only compiles the js and html points to the compiled

---
+++ webpack.config.js
@@ -0,0 +1,13 @@
+const path = require("path")
+
+module.exports = {
+    entry: {
+        clicker: "./bin/js/clicker.js",
+        options: "./bin/js/options.js"
+    },
+    output: {
+        path: path.resolve(__dirname, "bin", "dist"),
+        filename: "[name].bundle.js"
+    }
+
+}
